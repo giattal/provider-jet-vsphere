ef1b340d96fe2e1266eb2fbe0a2dcdec12292e3b
Tal Giat <talg@nayax.com>
2022-03-07 10:12:44 +0200

    change import reference

--- config/host/config.go
+++ config/host/config.go
@@ -9,5 +9,16 @@ func Configure(p *config.Provider) {
 		// we need to override the default group that terrajet generated for
 		// this resource, which would be "github"
 		r.ShortGroup = "host"
+		// Identifier for this resource is assigned by the provider. In other
+		// words it is not simply the name of the resource.
+		r.ExternalName = config.IdentifierFromProvider
+
+		// This resource need the repository in which branch would be created
+		// as an input. And by defining it as a reference to Repository
+		// object, we can build cross resource referencing. See
+		// repositoryRef in the example in the Testing section below.
+		r.References["repository"] = config.Reference{
+			Type: "github.com/crossplane-contrib/provider-jet-vsphere/apis/repository/v1alpha1.Repository",
+		}
 	})
 }
